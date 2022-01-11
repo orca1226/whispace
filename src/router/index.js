0ddc09c9e4e74289504eabb47c58cb75d50ba513
Lillian Lin <lillian2179@gmail.com>
2022-01-11 22:18:04 +0800

    更改 router mode

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   scrollBehavior (to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
